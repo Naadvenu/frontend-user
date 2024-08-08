522a2157904260880dc8a49b131b413034047394
komalrajbhar <komal.rajbhar@ratnamcollege.edu.in>
2024-08-09 01:24:56 +0530

    gallery view modified

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useGalleryData from './useGalleryData';
 import { motion, AnimatePresence } from "framer-motion";
 
@@ -10,6 +10,27 @@ interface GalleryComponentProps {
 
 const GalleryComponent: React.FC<GalleryComponentProps> = ({ category }) => {
     const { galleryData, loading, error } = useGalleryData();
+    const [selectedItem, setSelectedItem] = useState<{ media: string; title: string } | null>(null);
+
+    const handleImageClick = (item: { media: string; title: string }) => {
+        setSelectedItem(item);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedItem(null);
+    };
+
+    // Effect to manage body class for overflow
+    useEffect(() => {
+        if (selectedItem) {
+            document.body.classList.add('overflow-hidden');
+        } else {
+            document.body.classList.remove('overflow-hidden');
+        }
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+    }, [selectedItem]);
 
     if (loading) {
         return (
@@ -20,7 +41,8 @@ const GalleryComponent: React.FC<GalleryComponentProps> = ({ category }) => {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
-                ><div className="text-4xl font-bold">Loading...</div>
+                >
+                    <div className="text-4xl font-bold">Loading...</div>
                 </motion.div>
             </AnimatePresence>
         );
@@ -61,8 +83,8 @@ const GalleryComponent: React.FC<GalleryComponentProps> = ({ category }) => {
     }
 
     return (
-        <div className="gallery-container py-5 px-5 w-full min-h-screen">
-            <div id="gallery" className="container-fluid">
+        <div className="gallery-container py-5 px-5 w-full">
+            <div id="gallery" className={`container-fluid ${selectedItem ? 'blur-sm' : ''}`}>
                 <motion.div
                     initial="hidden"
                     whileInView="visible"
@@ -76,42 +98,83 @@ const GalleryComponent: React.FC<GalleryComponentProps> = ({ category }) => {
                     {filteredData.map((item) => (
                         <motion.div
                             key={item.id}
-                            className="img-item"
+                            className="relative mb-14"
                             initial={{ opacity: 0, scale: 0.8 }}
                             whileInView={{ opacity: 1, scale: 1 }}
                             transition={{ duration: 0.5, delay: 0.1 * Number(item.id) }}
                             viewport={{ once: true }}
                         >
-                            <div className="img-wrapper relative">
-                                {
-                                    item.media ? (
-                                        item.media.endsWith('.mp4') || item.media.endsWith('.mov') || item.media.endsWith('.avi') || item.media.endsWith('.wmv') ? (
+                            <div className="img-wrapper">
+                                {item.media ? (
+                                    item.media.endsWith('.mp4') ||
+                                        item.media.endsWith('.mov') ||
+                                        item.media.endsWith('.avi') ||
+                                        item.media.endsWith('.wmv') ? (
                                             <video
                                                 src={item.media}
-                                                className="video-responsive w-full h-auto mx-auto shadow-[0_0px_5px_0_rgba(0,0,0,1)] cursor-pointer transition-all duration-200"
+                                                className="w-full h-auto mx-auto shadow-md cursor-pointer transition-all duration-200"
                                                 controls
                                             >
                                                 Your browser does not support the video tag.
                                             </video>
                                         ) : (
-                                                <img
-                                                    src={item.media}
-                                                    className="img-responsive w-full h-auto mx-auto shadow-[0_0px_5px_0_rgba(0,0,0,1)] cursor-pointer transition-all duration-200"
-                                                    alt={item.title}
-                                                />
-                                            )
-                                    ) : (
-                                            <p>No media available!</p>
+                                            <img
+                                                src={item.media}
+                                                className="w-full h-auto mx-auto shadow-md cursor-pointer transition-all duration-200"
+                                                alt={item.title}
+                                                onClick={() => handleImageClick(item)}
+                                            />
                                         )
-                                }
-                                <div className="img-label absolute bottom-0 left-0 w-full bg-white dark:bg-blacksection shadow-[0_0px_5px_0_rgba(0,0,0,1)] px-2 py-2 text-start text-black dark:text-white text-sm leading-3.5">
-                                    {item.title}
-                                </div>
+                                ) : (
+                                    <p>No media available!</p>
+                                )}
                             </div>
                         </motion.div>
                     ))}
                 </motion.div>
             </div>
+
+            <AnimatePresence>
+                {selectedItem && (
+                    <motion.div
+                        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        onClick={handleCloseModal}
+                    >
+                        <div className="relative bg-white dark:bg-blacksection p-2 rounded-lg shadow-lg max-w-lg w-full" onClick={(e) => e.stopPropagation()}>
+                            <button
+                                className="absolute top-2 right-2 text-red-500 text-2xl"
+                                onClick={handleCloseModal}
+                            >
+                                &times;
+                            </button>
+                            {selectedItem.media.endsWith('.mp4') ||
+                            selectedItem.media.endsWith('.mov') ||
+                            selectedItem.media.endsWith('.avi') ||
+                            selectedItem.media.endsWith('.wmv') ? (
+                                <video
+                                    src={selectedItem.media}
+                                    className="max-w-full max-h-[80vh] rounded-sm"
+                                    controls
+                                >
+                                    Your browser does not support the video tag.
+                                </video>
+                            ) : (
+                                <img
+                                    src={selectedItem.media}
+                                    className="max-w-full mx-auto max-h-[60vh] rounded-sm"
+                                    alt={selectedItem.title}
+                                />
+                            )}
+                            <h2 className="mt-4 text-lg font-bold text-center">
+                                {selectedItem.title}
+                            </h2>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
